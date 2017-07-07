966b7414b7288560ea0a53864c6373d0862354f5
aumoraes <aumoraes@sp.r7.com>
2017-07-07 20:08:20 -0300

    terminando cap. 2 ( factory, builder )

---
+++ src/main/java/br/com/caelum/argentum/modelo/Candlestick.java
@@ -0,0 +1,61 @@
+package br.com.caelum.argentum.modelo;
+
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public final class Candlestick {
+	private final double abertura;
+	private final double fechamento;
+	private final double minimo;
+	private final double maximo;
+	private final double volume;
+	private final Calendar data;
+	public Candlestick(double abertura, double fechamento, double minimo, double maximo, double volume, Calendar data) {
+		this.abertura = abertura;
+		this.fechamento = fechamento;
+		this.minimo = minimo;
+		this.maximo = maximo;
+		this.volume = volume;
+		this.data = data;
+	}
+	
+	public boolean isAlta() {
+		return this.abertura < this.fechamento;
+	}
+	
+	public boolean isBaixa() {
+		return this.abertura > this.fechamento;
+	}		
+		
+	public double getAbertura() {
+		return abertura;
+	}
+	public double getFechamento() {
+		return fechamento;
+	}
+	public double getMinimo() {
+		return minimo;
+	}
+	public double getMaximo() {
+		return maximo;
+	}
+	public double getVolume() {
+		return volume;
+	}
+	public Calendar getData() {
+		return data;
+	}
+	public String getFormatedDate(){
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = getData().getTime();
+		return sdf.format(date);
+	}
+	
+	@Override
+	public String toString() {
+		return "[Abertura "+getAbertura()+", Fechamento "+getFechamento()+", Mínimo "+getMinimo()+", Máximo "+getMaximo()+", Volume "+getVolume()+", Data "+getFormatedDate()+"]";
+	}
+	
+}
